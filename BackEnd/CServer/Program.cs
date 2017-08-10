93a26d5398e28217ff3d9eaf9cf24eb42cdb5836
Leon Schiffler <leon.schiffler@outlook.com>
2017-08-10 15:07:23 -0700

    Got rid of JS server and made C# server

---
+++ BackEnd/CServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CServer
+{
+    class Program
+    {
+        public static IPAddress ipAddress = IPAddress.Parse("167.220.100.62");
+        static void Main(string[] args)
+        {
+            TcpListener myList = new TcpListener(ipAdress, 8000);
+            
+            //Display that the server is running
+            Console.WriteLine("Server running - Port: 8000");
+            Console.WriteLine("Local end point:" + myList.LocalEndpoint);
+            Console.WriteLine("Waiting for connections...");
+
+            Socket s = myList.AcceptSocket();
+
+        }
+    }
+}
