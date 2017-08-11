ed86f7e476196406bd2b5e0ea52741125245ca6f
Leon Schiffler <leon.schiffler@outlook.com>
2017-08-11 09:14:34 -0700

    Now using a WebAPI for everything

---
+++ LectureLiveWebAPI/LectureLiveWebAPI/Student Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LectureLiveWebAPI
+{
+    public class studentResponse
+    {
+        int response;
+        DateTime timestamp;
+        bool expired;
+        
+    }
+}
---
+++ LectureLiveWebAPI/LectureLiveWebAPI/StudentResponses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LectureLiveWebAPI
+{
+    public class Responses
+    {
+        List<studentResponse> resp;
+        DateTime classStarted;
+        string className;
+    }
+}
