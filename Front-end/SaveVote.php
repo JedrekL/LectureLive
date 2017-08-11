b8315ad6599e374678b5a7feeb7242220646deab
endurablepants <30906894+endurablepants@users.noreply.github.com>
2017-08-10 23:25:28 -0700

    php
    php action not working yet

---
+++ Front-end/SaveVote.php
@@ -0,0 +1,12 @@
+
+<html>
+<body>
+
+Your emoji is <?php echo $_GET["emoji"]; ?><br>
+Your comment is: <?php echo $_GET["comment"]; ?><br>
+<br>
+Go back to the <a href="studentvotepage.html">Vote</a> page
+
+</body>
+</html>
+
