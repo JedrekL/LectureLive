d3229bc0eaa502fc7066a8276433f6eb78481c85
endurablepants <30906894+endurablepants@users.noreply.github.com>
2017-08-10 23:38:50 -0700

    save vote
    still doesn't work, maybe PHP doesn't work on localhost?

--- Front-end/SaveVote.php
+++ Front-end/SaveVote.php
@@ -2,8 +2,24 @@
 <html>
 <body>
 
-Your emoji is <?php echo $_GET["emoji"]; ?><br>
-Your comment is: <?php echo $_GET["comment"]; ?><br>
+
+<?php
+// open the file "demosaved.csv" for writing
+$file = fopen('demosaved.csv', 'a');
+$emoji =  htmlspecialchars($_GET["emoji"];
+$comment =  htmlspecialchars($_GET["comment"]
+ 
+// save the column headers
+fputcsv($file, array($emoji, $comment));
+ 
+// Close the file
+fclose($file);
+
+?>
+
+
+Your emoji is <?php echo $"emoji"; ?><br>
+Your comment is: <?php echo $"comment"; ?><br>
 <br>
 Go back to the <a href="studentvotepage.html">Vote</a> page
 
